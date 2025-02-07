e050aa9d958aee52f218570a09e90adb3bf2b0ec
beif3ng <69723927+beif3ng@users.noreply.github.com>
2025-02-07 23:38:32 +0600

    Added CRUD for News

--- src/news/routers.py
+++ src/news/routers.py
@@ -3,18 +3,22 @@ Routers for news app
 """
 from typing import Sequence
 
-from dns.e164 import query
 from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 
-from .models import Category
-from .schemas import CategoryReadSchema, CategoryCreateSchema
+from .models import Category, News
+from .schemas import CategoryReadSchema, CategoryCreateSchema, NewsReadSchema, NewsCreateSchema
 
 from src.database import session as async_session
 
 category_router = APIRouter(
     prefix="/categories",
     tags=["Categories"]
+)
+
+news_router = APIRouter(
+    prefix="/news",
+    tags=["News"]
 )
 
 
@@ -70,16 +74,16 @@ async def delete_category(category_id: int) -> Category:
             raise HTTPException(status_code=404, detail="Category not found")
         await session.delete(category)
         await session.commit()
+        return category
 
 
-@category_router.put("/{category_id}", response_model=CategoryReadSchema)
 @category_router.put("/{category_id}", response_model=CategoryReadSchema)
 async def update_category(category_id: int, category: CategoryCreateSchema) -> Category:
     """
     Update category by id
     """
     async with async_session() as session:
-        query = select(Category).filter(Category.id == category_id)
+        query = select(Category).where(Category.id == category_id)
         result = await session.execute(query)
         old_category = result.scalar_one_or_none()
         if old_category is None:
@@ -99,7 +103,7 @@ async def partial_update_category(category_id: int, category: CategoryCreateSche
     Update category by id
     """
     async with async_session() as session:
-        query = select(Category).filter(Category.id == category_id)
+        query = select(Category).where(Category.id == category_id)
         result = await session.execute(query)
         old_category = result.scalar_one_or_none()
         if old_category is None:
@@ -112,3 +116,99 @@ async def partial_update_category(category_id: int, category: CategoryCreateSche
         await session.commit()
         await session.refresh(old_category)
         return old_category
+
+
+@news_router.get("", response_model=Sequence[NewsReadSchema])
+async def get_news(offset: int = 0, limit: int = 10) -> Sequence[News]:
+    """
+    Get all news
+    """
+    async with async_session() as session:
+        query = select(News).offset(offset).limit(limit)
+        result = await session.execute(query)
+        news = result.scalars().all()
+        return news
+
+
+@news_router.get("/{news_id}", response_model=NewsReadSchema)
+async def get_news(news_id: int) -> News:
+    """
+    Get news by id
+    """
+    async with async_session() as session:
+        query = select(News).where(News.id == news_id)
+        result = await session.execute(query)
+        news = result.scalar_one_or_none()
+        if news is None:
+            raise HTTPException(status_code=404, detail="News not found")
+        return news
+
+
+@news_router.post("", response_model=NewsReadSchema)
+async def create_news(news: NewsCreateSchema) -> News:
+    """
+    Create news
+    """
+    async with async_session() as session:
+        new_news = News(**news.dict())
+        session.add(new_news)
+        await session.commit()
+        await session.refresh(new_news)
+        return new_news
+
+
+@news_router.delete("/{news_id}", response_model=NewsReadSchema)
+async def delete_news(news_id: int) -> News:
+    """
+    Delete news by id
+    """
+    async with async_session() as session:
+        query = select(News).where(News.id == news_id)
+        result = await session.execute(query)
+        news = result.scalar_one_or_none()
+        if news is None:
+            raise HTTPException(status_code=404, detail="News not found")
+        await session.delete(news)
+        await session.commit()
+        return news
+
+
+@news_router.put("/{news_id}", response_model=NewsReadSchema)
+async def update_news(news_id: int, news: NewsCreateSchema) -> News:
+    """
+    Update news by id
+    """
+    async with async_session() as session:
+        query = select(News).where(News.id == news_id)
+        result = await session.execute(query)
+        old_news = result.scalar_one_or_none()
+        if old_news is None:
+            raise HTTPException(status_code=404, detail="News not found")
+
+        for field, value in news.dict().items():
+            setattr(old_news, field, value)
+
+        await session.commit()
+        await session.refresh(old_news)
+        return old_news
+
+
+@news_router.patch("/{news_id}", response_model=NewsReadSchema)
+async def partial_update_news(news_id: int, news: NewsCreateSchema) -> News:
+    """
+    Update news by id
+    """
+    async with async_session() as session:
+        query = select(News).where(News.id == news_id)
+        result = await session.execute(query)
+        old_news = result.scalar_one_or_none()
+        if old_news is None:
+            raise HTTPException(status_code=404, detail="News not found")
+
+        for field, value in news.dict().items():
+            if value:
+                setattr(old_news, field, value)
+
+        await session.commit()
+        await session.refresh(old_news)
+        return old_news
