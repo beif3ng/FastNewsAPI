8041a24ea1dbda9f38aea76c6848f70024568cc6
Azim <69723927+beif3ng@users.noreply.github.com>
2025-02-15 18:32:24 +0600

    Update architecture of apps

--- src/news/__init__.py
+++ src/news/__init__.py
@@ -2,9 +2,8 @@
 __init__.py
 """
 
-from .routers import category_router, news_router
+from .routers import news_router
 
 __all__ = [
-    "category_router",
     "news_router",
 ]
--- src/news/schemas.py
+++ src/news/schemas.py
@@ -8,25 +8,6 @@ from pydantic import BaseModel
 from typing import List
 
 
-class CategoryReadSchema(BaseModel):
-    """
-    Category read schema
-    """
-    id: int
-    name: str
-    created: datetime
-
-    class Config:
-        orm_mode = True
-
-
-class CategoryCreateSchema(BaseModel):
-    """
-    Category create schema
-    """
-    name: str
-
-
 class NewsReadSchema(BaseModel):
     """
     News read schema
@@ -40,7 +21,7 @@ class NewsReadSchema(BaseModel):
     category_id: int | None = None
 
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class NewsCreateSchema(BaseModel):
